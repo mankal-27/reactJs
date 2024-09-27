6675945a9fd7cf8adb458f1efe30f618226b58ba
mankal-27 <mnkalkutagi@gmail.com>
2024-09-27 20:11:38 +0530

    updated learning react fundamentals

---
+++ 11_learn/react-learn/src/ThemeToggle.jsx
@@ -0,0 +1,22 @@
+import  { useState, useEffect } from 'react';
+
+const ThemeToggle = () => {
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <button onClick={toggleTheme}>
+      Switch to {theme === 'light' ? 'dark' : 'light'} mode
+    </button>
+  );
+};
+
+export default ThemeToggle;
